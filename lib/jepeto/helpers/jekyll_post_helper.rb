2c774f84e9f95e7b2bb25f15d5db865479deea00
Alex Tsui <alextsui05@gmail.com>
2017-05-12 12:02:50 -0700

    Fix post title being turned into a slug

--- lib/jepeto/helpers/jekyll_post_helper.rb
+++ lib/jepeto/helpers/jekyll_post_helper.rb
@@ -1,6 +1,7 @@
 module Jepeto
   module JekyllPostHelper
-    def slugalize(string)
+    def slugalize(str)
+      string = str.dup
       string.gsub!(/[^\x00-\x7F]+/, '')   # Remove non-ASCII (e.g. diacritics).
       string.gsub!(/[^a-z0-9\-_\+]+/i, '-') # Turn non-slug chars into the separator.
       string.gsub!(/-{2,}/, '-')            # No more than one of the separator in a row.
